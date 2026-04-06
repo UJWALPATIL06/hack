858bd04fca01524b052599828877d2727f9d646b
Bhushan <bhushan@Bhushans-MacBook-Air.local>
2026-04-06 17:05:49 +0530

    fixed gitignore

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,16 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
-const computeRoute   = require("./routes/compute");
-const materials = require("./config/materials.config");
-const materialsRoute = require("./routes/materials");
+const computeRoute   = require("./routes/compute.js");
+const materialsRoute = require("./routes/materials.js");
 
 let port = 8080;
 
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
-app.use("/api/compute", computeRoute);
-app.use("/api/materials", materialsRoute);
+app.use("/api", computeRoute);
+app.use("/api", materialsRoute);
 
 app.get("/", (req, res)=>{
     res.send("backend running");
